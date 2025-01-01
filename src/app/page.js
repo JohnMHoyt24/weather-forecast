2a0fdfce03492f9c9149ccb7ad47a11d9d8364e0
JohnMHoyt24 <Jmhoyt6355@gmail.com>
2025-01-01 14:46:04 -0500

    Created components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Forecast from './components/forecast';
+import Search from './components/search';
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Search />
+      <Forecast />
     </div>
   );
 }
