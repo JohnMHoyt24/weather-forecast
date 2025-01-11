57b6e609ac545f932fbf0d83578c6f72082eb379
JohnMHoyt24 <Jmhoyt6355@gmail.com>
2025-01-11 15:09:36 -0500

    Created CSS and added date functions to search.js

--- src/app/components/search.js
+++ src/app/components/search.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import 'assets/App.css';
+
+/*
+  When a user types in the search bar, 
+  the search component will make a request
+  to the API, then the results will be rendered
+  in the Forecast component.
+*/
+
+function weatherData(){
+  //Initalize state variables for input and weather data
+  const [input, setInput] = useState('');
+  const [weather, setWeather] = useState({
+    loading: false,
+    data: {},
+    error: false,
+  });
+
+  const toDateFunction = () => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    const weekDays = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+  }
+}
 
 function Search() {
   return (
     <div>
       <h1>Search</h1>
+
     </div>
   );
 }
